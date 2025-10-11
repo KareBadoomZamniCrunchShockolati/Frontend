7d96e430d02e46ae44efff8c6f60bf7719a5ad5e
soroush1384akhavan <soroush1384.akhavan@gmail.com>
2025-10-11 19:27:43 +0330

    tstint

---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,21 @@
+import CustomInput  from "@/components/Custom/CustomInput";
+import { Button } from "@/components/ui/button";
+
+function Landing() {
+	return (
+		<div className="w-full h-screen flex flex-col items-center justify-center gap-4 bg-gray-50">
+			<h1 className="text-3xl font-bold mb-4 text-gray-800">
+				Welcome to My App 🚀
+			</h1>
+
+			<div className="flex items-center gap-2">
+				<CustomInput
+					className="w-64"
+				/>
+				<Button>Submit</Button>
+			</div>
+		</div>
+	);
+}
+
+export default Landing;
