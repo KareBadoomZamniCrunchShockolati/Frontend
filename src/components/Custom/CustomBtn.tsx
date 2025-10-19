0a1d428d0833d387322b004cd36aedae46fe78d5
Sina Fallahi <sinafallahi005@gmail.com>
2025-10-19 18:16:49 +0330

    added loading props for spinner

--- src/components/Custom/CustomBtn.tsx
+++ src/components/Custom/CustomBtn.tsx
@@ -3,6 +3,7 @@ import { Spinner } from "@/components/ui/spinner";
 import type { CustomBtnProps } from "@/types/btnTypes";
 export default function CustomBtn({
   color = "#fff",
+  loading = false,
   children,
   ...props
 }: CustomBtnProps) {
@@ -12,14 +13,15 @@ export default function CustomBtn({
         {...props}
         className="
         h-[40px] border-[2px] border-[#000] rounded-[15px] shadow-button active:shadow-button-active
-        font-bold cursor-pointer transition-all duration-100 active:translate-y-[3px]
+        font-bold cursor-pointer transition-all duration-30 active:translate-y-[3px]
         w-full bg-[#3871DD] mb-[20px] py-[3px] px-[39px] text-[20px]
         hover:bg-[#3871DD]
         disabled:opacity-50 disabled:cursor-not-allowed 
       "
         style={{ color }}
+        disabled={loading || props.disabled}
       >
-        {props.disabled ? (
+        {loading ? (
           <Spinner className="opacity-100" />
         ) : (
           <Spinner className="opacity-0" />
