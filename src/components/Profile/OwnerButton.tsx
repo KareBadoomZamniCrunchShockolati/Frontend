41d6b8d03ef385e189c597efb7965c5012ee940a
mahditd <149570042+mahditd@users.noreply.github.com>
2025-10-27 11:18:43 +0330

    refactor/profile fix button hovers

--- src/components/Profile/OwnerButton.tsx
+++ src/components/Profile/OwnerButton.tsx
@@ -14,13 +14,13 @@ const OwnerButton = () => {
   return (
     <div className="flex justify-around mr-5 ml-5 mt-5">
       <div>
-        <CustomButton backgroundColor="bg-secondary">
+        <CustomButton backgroundColor="bg-secondary hover:bg-secondary">
           ویرایش پروفایل
         </CustomButton>
       </div>
 
       <div>
-        <CustomButton onClick={() => setOpen(true)} backgroundColor="bg-primary">
+        <CustomButton onClick={() => setOpen(true)} backgroundColor="bg-primary hover:bg-primary">
           بساز+
         </CustomButton>
       </div>
@@ -38,10 +38,10 @@ const OwnerButton = () => {
                 done.
               </SheetDescription> */}
           </SheetHeader>
-          <div className="grid flex-1 justify-center auto-rows-min gap-6 px-1 mt-6">
-                <CustomButton pageAddress="/temp" backgroundColor="bg-primary" >پست جدید</CustomButton>
+          <div className="grid flex-1 justify-center auto-rows-min gap-6 px-1 mt-6 ">
+                <CustomButton pageAddress="/temp" backgroundColor="bg-primary hover:bg-primary" >پست جدید</CustomButton>
 
-                <CustomButton pageAddress="/temp" backgroundColor="bg-secondary" >چالش جدید</CustomButton>
+                <CustomButton pageAddress="/temp" backgroundColor="bg-secondary hover:bg-secondary" >چالش جدید</CustomButton>
           </div>
         </SheetContent>
       </Sheet>
