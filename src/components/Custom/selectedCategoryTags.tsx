cf0e6382e6f6970579cfecf419e770d9bff660b4
mahditd <149570042+mahditd@users.noreply.github.com>
2025-12-01 15:26:02 +0330

    refactore : make the svg remove buttons a sepaarate component

--- src/components/Custom/selectedCategoryTags.tsx
+++ src/components/Custom/selectedCategoryTags.tsx
@@ -1,14 +1,14 @@
 // components/Custom/SelectedCategoryTag.tsx
 import React from "react";
 import type { SelectedCategoryTagProps } from "@/types/challengeCreateTypes";
-
+import CategoryRemoveButton from "../ChallengeManagement/create/categoryRemoveButton";
 
 const AllSelectedCategoryTag: React.FC<SelectedCategoryTagProps> = ({
   category,
   onRemove,
 }) => {
   return (
-    <span className="inline-flex items-center gap-1 bg-orange-100 text-primary px-3 py-1 rounded-full text-sm">
+    <span className="inline-flex items-center gap-1 bg-primary-picture-background text-primary px-3 py-1 rounded-full text-sm">
       {category}
       <button
         type="button"
@@ -16,14 +16,7 @@ const AllSelectedCategoryTag: React.FC<SelectedCategoryTagProps> = ({
         className="hover:bg-primary-hover rounded-primary-radius p-0.5 transition-colors"
         aria-label={`حذف ${category}`}
       >
-        <svg className="w-3 h-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M6 18L18 6M6 6l12 12"
-          />
-        </svg>
+        <CategoryRemoveButton />
       </button>
     </span>
   );
