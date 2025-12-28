e201f7a17dbd86a81721d5871d37c55d9abf5bec
mahditd <149570042+mahditd@users.noreply.github.com>
2025-12-28 10:09:12 +0330

    refactor: add default challenge to data folder

--- src/pages/ChallengeInfo.tsx
+++ src/pages/ChallengeInfo.tsx
@@ -35,29 +35,11 @@ import {
 } from "@/services/userService";
 
 import CustomToast from "@/components/Custom/CustomToast";
-
 import { DEFAULT_CHALLENGE_IMG } from "@/data/mockImages";
-
-// ← ADD THIS IMPORT
 import { Spinner } from "@/components/ui/spinner";
+import { defaultChallengeData } from "@/data/mockChallenges";
 
-const defaultChallenge: ChallengeDataDetails = {
-  commentsEnabled: false,
-  categories: [],
-  type: "عمومی",
-  memberCount: "0",
-  title: "عنوان چالش",
-  description:
-    "این چالش برای آزمایش استقامت و مهارت‌های حل مسئله شما طراحی شده است. سفر شامل پیمودن زمین‌های سخت و غلبه بر موانع مختلف است. آیا آماده‌اید تا این ماجراجویی را شروع کنید و مرزهای خود را بسنجید؟",
-  dateRange: "از 28 اردیبهشت تا 8 شهریور - سه روز در هفته",
-  location: "قله کوه اورست",
-  Img: DEFAULT_CHALLENGE_IMG,
-  participants: [],
-  like_count: 0,
-  start_time: "28 اردیبهشت",
-  end_time: "8 شهریور",
-  visibility: "public",
-};
+const defaultChallenge = defaultChallengeData
 
 const ChallengeInfo: React.FC = () => {
   const location = useLocation();
